4f0f7c4d1d61feb35f00925a6402716c5291b478
deepakantony <deepakantony@gmail.com>
2011-09-02 14:14:40 -0700

    combination problem is solved

--- algorithms/recursion/combination/Combination.h
+++ algorithms/recursion/combination/Combination.h
@@ -4,16 +4,56 @@
 #include <string>
 #include <vector>
 
-template < class ElemType, class ElemContainerType > bool findCombinations(
-	const ElemType &combineMe, 
-	ElemContainerType &combinationList
-	);
-/*
-bool findCombinations(
-	std::string combineMe, 
-	std::vector<std::string> combinationList
-	);
-*/
+using namespace std;
+
+template < class ElemType, class ElemContainerType > 
+	bool findCombinations(
+		const ElemType &combineMe, 
+		ElemContainerType &combinationList
+		);
+
+template <class ElemType, class ElemContainerType>
+	bool findCombinations(
+		const ElemType &combineMe,
+		ElemContainerType &combinationList,
+		ElemType &currentItem,
+		int currentLoc
+		);
+
+template < class ElemType, class ElemContainerType > 
+	bool findCombinations(
+		const ElemType &combineMe, 
+		ElemContainerType &combinationList
+		)
+{
+	ElemType currentItem;
+	return findCombinations(combineMe, combinationList, currentItem, 0);
+}
+
+template <class ElemType, class ElemContainerType>
+	bool findCombinations(
+		const ElemType &combineMe,
+		ElemContainerType &combinationList,
+		ElemType &currentItem,
+		int startLoc
+		)
+{
+	for(int currentLoc = startLoc; currentLoc < combineMe.size(); currentLoc++) 
+	{
+		// use the current item
+		currentItem.push_back(combineMe[currentLoc]);
+					
+		// add it the combinations list
+		combinationList.push_back(currentItem);
+
+		// recurse to find more combinations 
+		findCombinations(combineMe, combinationList, currentItem, currentLoc+1);
+			
+		// once used we should unuse it
+		currentItem.erase(currentItem.size()-1);
+	}
+	return true;
+}
 
 #endif // _COMBINATION_H_
 
