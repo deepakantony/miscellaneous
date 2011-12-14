260007db01b880c81bcfa32253973eabd56f9e54
Deepak Antony <deepakantony@gmail.com>
2011-12-14 10:50:49 -0800

    solved largest sum consecutive sequence

--- python_toy_programs/Strings/StringUtilFuncs.py
+++ python_toy_programs/Strings/StringUtilFuncs.py
@@ -6,4 +6,18 @@ def printString(string):
 
 # first non repeated char
 def firstNonRepeatesChar(string):
+    pass
+
+# largest sum consecutive sequence
+def largestSumConsecutiveSeq(aList, start, end):
+    #TODO:error check for bounds
+    
+
+    # assume there is atleast one positive integer
+    maxSum = [max(0,aList[start])]
+    for index in xrange(start+1, end):
+        maxSum.append(max(0, maxSum[index-1]+aList[index]))
+
+    return max(maxSum)
+
     
