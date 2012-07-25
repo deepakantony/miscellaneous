93a348fe33e616391027d67bc0ffee47282a1bd2
Deepak Antony <deepakantony@gmail.com>
2012-07-25 14:34:26 -0700

    completed the ruby helloworld program

---
+++ learning/book_lrthw/01_AGoodFirstProgram.rb
@@ -0,0 +1,7 @@
+puts "I'm awesome"
+puts "I know # will make the line comment"
+puts 'I can also do "this"'
+puts "and 'this'"
+puts 'Does this count as another line'
+puts "I did learn that # is called octothorpe"
+
