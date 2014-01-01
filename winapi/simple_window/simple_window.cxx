3762cc414b1cf920c51fad6d5df398eb6254f036
Deepak <deepakantony@gmail.com>
2014-01-01 03:54:17 -0800

    added a menu bar with exit and a go menu

--- winapi/simple_window/simple_window.cxx
+++ winapi/simple_window/simple_window.cxx
@@ -2,6 +2,25 @@
 
 const char g_szClassName[] = "myWindowClass";
 
+#define ID_FILE_EXIT 9001
+#define ID_STUFF_GO  9002
+
+/* Get menu bar */
+HMENU GetMenuBar()
+{
+    HMENU hmenu = CreateMenu();
+
+    HMENU hfilepopup = CreateMenu();
+    AppendMenu( hfilepopup, MF_STRING, ID_FILE_EXIT, "E&xit" );
+    AppendMenu( hmenu, MF_STRING | MF_POPUP, ( UINT ) hfilepopup, "&File" );
+
+    HMENU hstuffpopup = CreateMenu();
+    AppendMenu( hstuffpopup, MF_STRING, ID_STUFF_GO, "&Go" );
+    AppendMenu( hmenu, MF_STRING | MF_POPUP, ( UINT ) hstuffpopup, "&Stuff" );
+
+    return hmenu;
+}
+
 LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam )
 {
     switch ( msg )
@@ -14,6 +33,17 @@ LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam )
         MessageBox( hwnd, szFileName, "The Program is: ", MB_OK | MB_ICONINFORMATION );
         break;
     }
+    case WM_COMMAND:
+        switch ( LOWORD( wParam ) )
+        {
+        case ID_FILE_EXIT:
+            PostMessage( hwnd, WM_CLOSE, 0, 0 );
+            break;
+        case ID_STUFF_GO:
+            MessageBox( hwnd, "Stuff Go Menu Dialog Box", "GO", MB_OK | MB_ICONINFORMATION );
+            break;
+        }
+        break;
     case WM_CLOSE:
         DestroyWindow( hwnd );
         break;
@@ -52,7 +82,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
         return 0;
     }
 
-    hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, g_szClassName, "Simple Window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 512, 256, NULL, NULL, hInstance, NULL );
+    hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, g_szClassName, "Simple Window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 512, 256, NULL, GetMenuBar(), hInstance, NULL );
 
     if ( hwnd == NULL )
     {
