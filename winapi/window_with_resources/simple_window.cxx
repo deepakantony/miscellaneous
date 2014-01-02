53670ee3f34f7d579f0f741c67c797e1435cef42
Deepak <deepakantony@gmail.com>
2014-01-01 22:05:52 -0800

    window with a about menu item using resources

---
+++ winapi/window_with_resources/simple_window.cxx
@@ -0,0 +1,137 @@
+#include <windows.h>
+#include "resource.h"
+
+const char g_szClassName[] = "myWindowClass";
+
+#define ID_FILE_EXIT    9001
+#define ID_STUFF_GO     9002
+#define ID_STUFF_ABOUT  9003
+
+BOOL CALLBACK AboutProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+
+    switch ( msg )
+    {
+    case WM_INITDIALOG:
+        break;
+    case WM_COMMAND:
+        switch ( LOWORD( wParam ) )
+        {
+        case IDOK:
+            EndDialog( hwnd, IDOK );
+            break;
+        case IDCANCEL:
+            EndDialog( hwnd, IDCANCEL );
+            break;
+        }
+    default:
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+/* Get menu bar */
+HMENU GetMenuBar()
+{
+    HMENU hmenu = CreateMenu();
+
+    HMENU hfilepopup = CreateMenu();
+    AppendMenu( hfilepopup, MF_STRING, ID_FILE_EXIT, "E&xit" );
+    AppendMenu( hmenu, MF_STRING | MF_POPUP, ( UINT ) hfilepopup, "&File" );
+
+    HMENU hstuffpopup = CreateMenu();
+    AppendMenu( hstuffpopup, MF_STRING, ID_STUFF_GO, "&Go" );
+    AppendMenu( hstuffpopup, MF_STRING, ID_STUFF_ABOUT, "&About" );
+    AppendMenu( hmenu, MF_STRING | MF_POPUP, ( UINT ) hstuffpopup, "&Stuff" );
+
+    return hmenu;
+}
+
+LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+    switch ( msg )
+    {
+    case WM_LBUTTONDOWN:
+    {
+        char szFileName[ MAX_PATH ];
+        HINSTANCE hInstance = GetModuleHandle( NULL );
+        GetModuleFileName( hInstance, szFileName, MAX_PATH );
+        MessageBox( hwnd, szFileName, "The Program is: ", MB_OK | MB_ICONINFORMATION );
+        break;
+    }
+    case WM_COMMAND:
+        switch ( LOWORD( wParam ) )
+        {
+        case ID_FILE_EXIT:
+            PostMessage( hwnd, WM_CLOSE, 0, 0 );
+            break;
+        case ID_STUFF_GO:
+            MessageBox( hwnd, "Stuff Go Menu Dialog Box", "GO", MB_OK | MB_ICONINFORMATION );
+            break;
+        case ID_STUFF_ABOUT:
+            INT_PTR ret = DialogBox( GetModuleHandle( NULL ), MAKEINTRESOURCE( IDD_DIALOG1 ), hwnd, AboutProc );
+            if ( ret <= 0 )
+            {
+                MessageBox( hwnd, "Dialog box errored out!", "Dialog box error", MB_OK | MB_ICONERROR );
+            }
+            break;
+        }
+        break;
+    case WM_CLOSE:
+        DestroyWindow( hwnd );
+        break;
+    case WM_DESTROY:
+        PostQuitMessage( 0 );
+        break;
+    default:
+        return DefWindowProc( hwnd, msg, wParam, lParam );
+    }
+
+    return 0;
+}
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG msg;
+
+    wc.cbSize = sizeof( WNDCLASSEX );
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon( NULL, IDI_APPLICATION );
+    wc.hCursor = LoadCursor( NULL, IDC_ARROW );
+    wc.hbrBackground = ( HBRUSH ) ( COLOR_WINDOW + 1 );
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm = LoadIcon( NULL, IDI_APPLICATION );
+
+    if ( !RegisterClassEx( &wc ) )
+    {
+        MessageBox( NULL, "Window registration failed", "ERROR!", MB_ICONEXCLAMATION | MB_OK );
+        return 0;
+    }
+
+    hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, g_szClassName, "Simple Window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 512, 256, NULL, GetMenuBar(), hInstance, NULL );
+
+    if ( hwnd == NULL )
+    {
+        MessageBox( NULL, "Window creation failed", "ERROR!", MB_ICONEXCLAMATION | MB_OK );
+        return 0;
+    }
+
+    ShowWindow( hwnd, nCmdShow );
+    UpdateWindow( hwnd );
+
+    while ( GetMessage( &msg, NULL, 0, 0 ) > 0 )
+    {
+        TranslateMessage( &msg );
+        DispatchMessage( &msg );
+    }
+
+    return 0;
+}
